39d04b41886ecd3fd34827249e7d9338fef6d8d8
Hinko Kocevar <hinko.kocevar@esss.se>
2017-09-22 15:49:43 +0200

    Updates and new packages.
    Use github instead bitbucket.

---
+++ packages/test1/ioc1/ioc1App/src/O.linux-x86_64/ioc1Main.d
@@ -0,0 +1,16 @@
+ioc1Main.o: ../ioc1Main.cpp \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/epicsExit.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/shareLib.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/epicsThread.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/shareLib.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/epicsEvent.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/os/Linux/osdEvent.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/epicsMutex.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/epicsAssert.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/compilerDependencies.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/compiler/gcc/compilerSpecific.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/os/Linux/osdMutex.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/os/Linux/osdThread.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/ellLib.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/epicsEvent.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/iocsh.h
---
+++ packages/test1/ioc1/ioc1App/src/O.linux-x86_64/ioc1_registerRecordDeviceDriver.d
@@ -0,0 +1,22 @@
+ioc1_registerRecordDeviceDriver.o: ioc1_registerRecordDeviceDriver.cpp \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/compilerDependencies.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/compiler/gcc/compilerSpecific.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/epicsStdlib.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/shareLib.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/os/Linux/osdStrtod.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/epicsTypes.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/compilerDependencies.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/errMdef.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/iocsh.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/iocshRegisterCommon.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/registryCommon.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/dbStaticLib.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/dbFldTypes.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/dbBase.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/ellLib.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/dbDefs.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/link.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/cantProceed.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/devSup.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/registryRecordType.h \
+ /home/hinkokocevar/ng3e/root/R3.15.4/base/include/recSup.h
